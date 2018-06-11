887737bb55e91fdb9f73c16af516c26ce4cebc98
Jan Skála <jan.skala566@gmail.com>
2018-06-12 01:34:44 +0200

    added google map to contact activity

--- feature/src/main/java/com/kjs566/kockafe/feature/ContactActivity.java
+++ feature/src/main/java/com/kjs566/kockafe/feature/ContactActivity.java
@@ -2,14 +2,38 @@ package com.kjs566.kockafe.feature;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.kjs566.kockafe.base.BaseActivity;
 
-public class ContactActivity extends BaseActivity {
+public class ContactActivity extends BaseActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.contact);
         setBackgroundImage(R.drawable.contact_background);
         inflateContent(R.layout.activity_contact_content);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
+
