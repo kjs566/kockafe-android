076576e13b53436a8c620c3cbc1016752c360af5
Jan Skála <jan.skala566@gmail.com>
2018-06-11 02:58:34 +0200

    added about activity and background

---
+++ feature/src/main/java/com/kjs566/kockafe/feature/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.kjs566.kockafe.feature;
+
+import android.os.Bundle;
+
+import com.kjs566.kockafe.base.BaseActivity;
+
+public class AboutActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        inflateContent(R.layout.activity_about_content);
+        setTitle(R.string.about_title);
+    }
+}
